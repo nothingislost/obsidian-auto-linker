239b3a3ca495840a594334ccb960aa5bd1eb4486
NothingIsLost <nothingislost@fastmail.net>
2021-12-15 13:38:51 -0800

    improve alias resolve logic

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 This is an early alpha of a plugin that allows for aliases to be referenced directly like `[[this]]` rather than `[[filename|this]]`
 
+In order to accomplish this, the plugin patches the default Obsidian link resolution logic. The logic will remain the same if the link resolves to an existing file in the vault. The new behavior is that, if no existing file is found, it will attempt to find a document containing a matching alias.
+
+The logic will pick the closest and first document with a matching alias. If you have multiple documents with the same alias, the first and closest document will always be resolved. If you want to maintain multiple documents with the same alias, it is recommended that you continue to fully qualify them as normal [[file|alias]].
+
+If you have an actual file with the same name as one of your aliases, the actual file will always be preferred. In the future, there may be an option to always prefer aliases.
+
+If you notice any weirdness with link resolution or the graph view, just disable this plugin and everything will revert back to the default resolution logic.
+
+### Components that now support bare aliases
+- Graph view
+- Backlinks
+- Embeds
+- Resolved/Unresolved link display in preview mode
+
+In theory, this patch may automatically allow other plugins, like DataView, to resolve bare aliases but that has not yet been tested.
+
 ### Installing via BRAT
 
 Install the BRAT plugin via the Obsidian Plugin Browser and then add the beta repository "nothingislost/obsidian-auto-linker"
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,82 @@
-import { Plugin, WorkspaceLeaf } from "obsidian";
+import { Plugin, MetadataCache } from "obsidian";
 import { around } from "monkey-around";
 
+declare module "obsidian" {
+  interface MetadataCache {
+    initialized: boolean;
+    initialize(): void;
+    getLinkSuggestions(): any[];
+  }
+}
+
 export default class ObsidianAutoLinkerPlugin extends Plugin {
+  aliasCache: any[];
+  patchMDCacheUninstaller: () => void;
+
   async onload() {
-    const patchOpenFile = around(WorkspaceLeaf.prototype, {
-      openLinkText(old: any) {
-        return function (baseName, refPath, props) {
-          // don't crash the default method
+    const plugin = this;
+    this.aliasCache = [];
+
+    this.patchMDCacheUninstaller = around(MetadataCache.prototype, {
+      // Here we patch the logic of getFirstLinkpathDest to resolve aliases
+      //   and treat them with top priority
+      // This means that if an alias name exists,it will always take priority
+      //   over an actual file with the same name.
+      getFirstLinkpathDest(old: any) {
+        return function (link, ctxPath) {
+          const result = old.call(this, link, ctxPath);
+          if (result) {
+            return result;
+          }
           try {
-            // TODO: look into ways to possibly optimize this
-            // with a vault of 3k files, this takes 3ms so it might be fine
-            const alias = this.app.metadataCache
-              .getLinkSuggestions()
-              .find(a => a.alias?.toLowerCase() === baseName.toLowerCase());
-            if (alias?.file?.basename) {
-              baseName = alias.file.basename;
-              return old.call(this, baseName, refPath, props);
+            // don't crash the method!
+            const alias = plugin.getFileByAlias(link);
+            if (alias) {
+              return alias.file;
+            } else {
+              return null;
             }
-          } catch {}
-          return old.call(this, baseName, refPath, props);
+          } catch {
+            return null;
+          }
         };
       },
-    }); 
-    this.register(patchOpenFile);
+    });
+    this.register(this.patchMDCacheUninstaller);
+
+    // If the cache has already been inialized, that most likely means
+    //    that it was loaded before we were able to patch.
+    // Rerun init so that the cache gets updated.
+    if (this.app.metadataCache.initialized) {
+      // This call is overkill but it reloads the unresolved and resolved caches
+      // the call is async and takes around 200ms on a ~3k file vault
+      this.app.metadataCache.initialize();
+    }
+  }
+
+  async onunload() {
+    this.patchMDCacheUninstaller();
+    this.app.metadataCache.initialize();
+  }
+
+  getFileByAlias(alias: string) {
+    // populate an emphemeral alias cache so that we don't slow this method down horribly
+    // the cache lives for 2 seconds before being cleared
+    this.populatAliasCache();
+    return this.aliasCache.find(a => a.alias?.toLowerCase() === alias.toLowerCase());
+  }
+
+  clearAliasCache() {
+    setTimeout(() => {
+      this.aliasCache = [];
+    }, 2000);
+  }
+
+  populatAliasCache() {
+    // on a vault of ~3k files, this takes around 3ms to populate
+    if (!this.aliasCache.length) {
+      this.aliasCache = this.app.metadataCache.getLinkSuggestions().filter(file => file.alias);
+      this.clearAliasCache();
+    }
   }
 }
